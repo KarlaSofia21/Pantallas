233235ffe88513424174b2d417bb1f52bfbac1f6
KarlaSofia21 <sofiadrew432@gmail.com>
2025-02-26 17:01:59 -0600

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+import Login from './app/Screens/Login';
+import Tienda from './app/Screens/Tienda';
+import ListaProductos from './app/Screens/ListaProductos';
+import Pronostico from './app/Screens/Pronostico';
+import LoginCuna from './app/Screens/LoginCuna';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(LoginCuna);
